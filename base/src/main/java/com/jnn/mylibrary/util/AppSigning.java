71ace538a338ab0b106d510e77e841a14eb903c7
jnn <jnn_dream@126.com>
2019-06-13 14:31:21 +0800

    [添加工具类]获取签名信息

---
+++ base/src/main/java/com/jnn/mylibrary/util/AppSigning.java
@@ -0,0 +1,96 @@
+package com.jnn.mylibrary.util;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author jnn
+ * @date 2019/6/13
+ * @description 获取应用签名信息
+ * 应用场景：微信登录/分享等三方注册需要用到 App签名信息
+ * 优势：直接通过代码获取，方便赋值
+ */
+public class AppSigning {
+    public final static String MD5 = "MD5";
+    public final static String SHA1 = "SHA1";
+    public final static String SHA256 = "SHA256";
+
+    /**
+     * 返回一个签名的对应类型的字符串
+     *
+     * @param context
+     * @param packageName
+     * @param type
+     * @return
+     */
+    public static String getSingInfo(Context context, String packageName, String type) {
+        String result = "error!";
+        try {
+            Signature[] signs = getSignatures(context, packageName);
+            for (Signature sig : signs) {
+                String tmp = "error!";
+                if (MD5.equals(type)) {
+                    tmp = getSignatureString(sig, MD5);
+                } else if (SHA1.equals(type)) {
+                    tmp = getSignatureString(sig, SHA1);
+                } else if (SHA256.equals(type)) {
+                    tmp = getSignatureString(sig, SHA256);
+                }
+                result = tmp;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 返回对应包的签名信息
+     *
+     * @param context
+     * @param packageName
+     * @return
+     */
+    public static Signature[] getSignatures(Context context, String packageName) {
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = context.getPackageManager().getPackageInfo(packageName, PackageManager.GET_SIGNATURES);
+            return packageInfo.signatures;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 获取相应的类型的字符串（把签名的byte[]信息转换成16进制）
+     *
+     * @param sig
+     * @param type
+     * @return
+     */
+    public static String getSignatureString(Signature sig, String type) {
+        byte[] hexBytes = sig.toByteArray();
+        String fingerprint = "error!";
+        try {
+            MessageDigest digest = MessageDigest.getInstance(type);
+            if (digest != null) {
+                byte[] digestBytes = digest.digest(hexBytes);
+                StringBuilder sb = new StringBuilder();
+                for (byte digestByte : digestBytes) {
+                    sb.append((Integer.toHexString((digestByte & 0xFF) | 0x100)).substring(1, 3));
+                }
+                fingerprint = sb.toString();
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return fingerprint;
+    }
+}
