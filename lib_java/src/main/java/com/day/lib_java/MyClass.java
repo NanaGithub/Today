58a3c764baa51972194befa6d665da64e08295ac
jnn <jnn_dream@126.com>
2019-06-16 16:48:58 +0800

    fix

--- lib_java/src/main/java/com/day/lib_java/MyClass.java
+++ lib_java/src/main/java/com/day/lib_java/MyClass.java
@@ -2,6 +2,16 @@ package com.day.lib_java;
 
 public class MyClass {
     public static void main(String[] args) {
-        System.out.print("adsda");
+        StringBuilder sb = new StringBuilder();
+        StringBuffer sbu = new StringBuffer();
+
+        String str = "sada";
+        char[] c = new char[]{'1', '2', '4', '4'};
+        char[] toC = new char[]{'0', '0', '0', '0', '0', '0'};
+        str.getChars(0, 3, c, 1);
+        System.out.print(c);
+        System.arraycopy(c, 0, toC, 0, 4);
+        System.out.print(toC);
+
     }
 }
